9666cfaf77cf9cd92b802ebcf313b76aaa485eba
noogyung <37189970+noogyung@users.noreply.github.com>
2021-12-28 03:08:18 +0900

    First Commit

---
+++ src/main/java/me/noogs/smartblastfurnace.java
@@ -0,0 +1,39 @@
+package me.noogs;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class smartblastfurnace extends JavaPlugin {
+
+    FileConfiguration config;
+    File cfile;
+    private static smartblastfurnace plugin;
+
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Smart Blast Furnace Plugin has Started.");
+
+        plugin = this;
+
+        config = getConfig();
+        config.options().copyDefaults();
+        saveConfig();
+        cfile = new File(getDataFolder(), "config.yml");
+
+        //getCommand("smartbf").setExecutor(new smartbfCommands());
+
+        //Event
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Smart Blast Furnace Plugin has Stopped.");
+    }
+
+    public static smartblastfurnace getPlugin(){return plugin;}
+}
